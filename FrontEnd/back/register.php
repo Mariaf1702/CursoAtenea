61f4788f197215ce4af6793395b594303251c3df
Mariaf1702 <143852427+Mariaf1702@users.noreply.github.com>
2023-11-10 15:06:45 -0500

    Add files via upload

--- FrontEnd/back/register.php
+++ FrontEnd/back/register.php
@@ -9,15 +9,30 @@
         $idperson= $_POST['idperson'];
         $email= $_POST['email'];
         $pass= $_POST['pass'];
-        
+
+        //Para darle un poco de seguridad a los datos se encodifican los datos
+        $pass_en = base64_encode($pass);
+
+        //Consultar si hay algun registro con el id ingresado
+        $consulta = mysqli_query($conexion, "SELECT * FROM users where idperson = '$idperson'");
+        //Cuentas la cantidad de columnas que traer la anterioir consulta 
+        $cantidad = mysqli_num_rows($consulta);
+
+        if ($cantidad ==1) {    // Significa que ya hay un registro con el numero de id ingresado
+            header('location:../index.php?status=2');
+        }else{ // Realice la ejecucion del regitro
         //Para hacer consultas recibe dos parametros
         //mysqli_query("conexion de la bd", "consulta")
         //Consulta inserta cada variable en la bd
         mysqli_query($conexion," INSERT INTO users
         (names, lastname, birthdate, idperson, email, pass) VALUES        
-        ('$names', '$lastname', '$birthdate', '$idperson', '$email', '$pass')");
+        ('$names', '$lastname', '$birthdate', '$idperson', '$email', '$pass_en')");
 
         header('location:../index.php?status=1');
+
+        }
+        
+        
 
     }
 ?>
