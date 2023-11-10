86d5238d4dad63e27e689f02bc03fb3175429cf1
Mariaf1702 <143852427+Mariaf1702@users.noreply.github.com>
2023-11-10 14:54:10 -0500

    Add files via upload

---
+++ BackEnd/ejercicio5/db/conexion.php
@@ -0,0 +1,17 @@
+<?php
+/*Archivo de conexion a base de datos*/
+$server ="localhost";
+$userdb = "root";
+$passdb = "";
+$dbname= "mi_proyecto";
+//mysqli ("Servidor","nombre de usuario de la BD", "contraseña", "A que base de datos vamos a apuntar")
+$conexion = new mysqli($server,$userdb, $passdb ,$dbname); // mysqli nos permite hacer una conexion
+// Si hay un error en la conexion
+if ($conexion -> connect_errno) {
+    echo "error en la conexión";
+    exit();
+}else {
+    echo "Si se conecto a la DB";
+}
+
+?>
