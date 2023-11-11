604f761ca4461bd4a2e41ec591227e8cf2f844df
Mariaf1702 <143852427+Mariaf1702@users.noreply.github.com>
2023-11-10 19:16:41 -0500

    Add files via upload

--- BackEnd/ejercicio5/index.php
+++ BackEnd/ejercicio5/index.php
@@ -1,27 +1,60 @@
+<?php
+    include 'db/conexion.php';
+
+    $query = mysqli_query($conexion, "SELECT * from usuarios")
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-
-    <!-- Boostrap -->
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-
-    <title>Punto 3 <BackEnd</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <title>Punto 5</title>
 </head>
 <body>
-        <h1>Aca puedes consultar la base de datos</h1>
-        <form action="ejercicio6.php" method="POST">
-            <div class="col-sm-10">
-                <button type="submit" name ="enviar" class="btn btn-primary">Enviar </button>
-            </div>                
-        </form>
-</body>
+    <script src="js/script.js"></script>
 
+    <section class="container">
+        <h2>Tabla punto 5</h2>
+        <input type="search" class="form-control table-filter" placeholder="Search..." />
+
+        <div class="table-responsive">
+            <table role="table" class="table filtered-table">
+            <thead role="rowgroup">
+                <tr role="row">
+                <th role="columnheader">#</th>
+                <th role="columnheader">id</th>
+                <th role="columnheader">Nombre</th>
+                <th role="columnheader">Apellido</th>
+                <th role="columnheader">Cédula</th>
+
+                </tr>
+            </thead>
+
+            <?php
+                while($datos = mysqli_fetch_array($query)){
+                    $id = $datos['id'];
+                    $nombre = $datos['nombre'];
+                    $apellido = $datos['apellido'];
+                    $cedula = $datos['cedula'];
+
+                    echo '
+                    <tbody role="rowgroup">
+                        <tr role="row">
+                            <td></td>
+                            <td>'.$id.'</td>
+                            <td>'.$nombre.'</td>
+                            <td>'.$apellido.'</td>
+                            <td>'.$cedula.'</td>        
+                    ';
+                }            
+            ?>
+            </table>
+        </div>
+        </section>
+    
+</body>
 </html>
